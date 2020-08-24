500835f1f113bfee240f35327eb17a73ad6360be
dengrixiong <dengrixiong@wanda.cn>
2020-08-24 23:02:14 +0800

    init framework project

---
+++ common/src/main/java/cn/sean/framework/common/core/IntArrayValuable.java
@@ -0,0 +1,16 @@
+package cn.sean.framework.common.core;
+
+/**
+ * @author Administrator
+ * @version v 1.0
+ * @title [title]
+ * @description [description]
+ * @company WANDA CLOUD
+ * @create 2020/8/24 0024 22:48
+ */
+public interface IntArrayValuable {
+    /**
+     * @return int 数组
+     */
+    int[] array();
+}
